4b9219b8d04b501a612d2549bbb66fd7e90edfa9
nagarjun <selvaprabhu.nagarjun@gmail.com>
2021-08-20 11:32:58 +0530

    DB Connection MySQL Server

---
+++ Ecommerce/Model/Products.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.ComponentModel.DataAnnotations;
+
+namespace Ecommerce.Model
+{
+    public class Products
+    {
+        [Key]
+
+        public int Id { get; set; }
+        [Required]
+
+        public string ProductName { get; set; }
+
+        [Required]
+        public int ProductAmout { get; set; }
+
+        public int ProductQuantity { get; set; }
+
+        public DateTime Created { get; set; }
+    }
+}
