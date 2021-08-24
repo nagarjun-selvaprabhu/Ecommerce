8b979c91a3fe3d15784869d4b0675454749f9675
nagarjun <selvaprabhu.nagarjun@gmail.com>
2021-08-24 14:00:50 +0530

    Added CRUD Endpoints for Product

--- Ecommerce/Controllers/ProductsController.cs
+++ Ecommerce/Controllers/ProductsController.cs
@@ -47,7 +47,7 @@ namespace Ecommerce.Controllers
         {
             try
             {
-                logger.Information("Information is logged");
+                logger.Information("Inside GetAllProducts method");
                 var cacheKey = "GET_ALL_PRODUCTS";
 
                 if (_cache.TryGetValue(cacheKey, out List<Products> products))
@@ -59,7 +59,7 @@ namespace Ecommerce.Controllers
                     var myTask = Task.Run(() => _productRepo.GetAllProducts());
                     var objList = await myTask;
                     _cache.Set(cacheKey, objList);
-                    logger.Information("Information is logged");
+                    logger.Information("GetAllProducts method Exited");
                     return Ok(objList);
                 }
             }
@@ -70,6 +70,92 @@ namespace Ecommerce.Controllers
             }
         }
 
+        [HttpGet("{ProductsId:int}", Name = "GetProduct")]
+        [ProducesResponseType(200, Type = typeof(Products))]
+        [ProducesResponseType(404)]
+        [ProducesDefaultResponseType]
+        public IActionResult GetProduct(int ProductsId)
+        {
+            try
+            {
+                var obj = _productRepo.GetProduct(ProductsId);
+                if (obj == null)
+                {
+                    return NotFound();
+                }
+                return Ok(obj);
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Something went wrong: {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPost]
+        [ProducesResponseType(201, Type = typeof(Products))]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public async Task<IActionResult> CreateProducts([FromBody] Products products)
+        {
+            try
+            {
+                if (products == null)
+                {
+                    return BadRequest();
+                }
+                if (_productRepo.ProductExists(products.ProductName))
+                {
+                    ModelState.AddModelError("", "National Park Exists!");
+                    return StatusCode(404, ModelState);
+                }
+                var flag = _productRepo.CreateProduct(products);
+                if (flag)
+                {
+                    return Ok("successfully created");
+                }
+                else
+                {
+                    return StatusCode(404, ModelState);
+                }
+            }
+            catch (Exception ex)
+            {
+                logger.Error($"Something went wrong: {ex}");
+                return StatusCode(500, "Internal server error");
+            }
+        }
+
+        [HttpPatch("{ProductsId:int}", Name = "UpdateProductsk")]
+        [ProducesResponseType(204)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+        public IActionResult UpdateProductsk(int ProductsId, [FromBody] Products products)
+        {
+            try
+            {
+                if (products == null || ProductsId != products.Id)
+                {
+                    return BadRequest(ModelState);
+                }
+                var flag = _productRepo.UpdateProduct(products);
+                if (flag)
+                {
+                    return Ok("successfully updated");
+                }
+                else
+                {
+                    return StatusCode(404, ModelState);
+                }
+            }
+            catch(Exception e)
+            {
+                logger.Error($"Something went wrong: {e}");
+                return StatusCode(500, "Internal server error");
+            }
+
+        }
 
 
     }
